0615c90c3923398eab88308b153caa368981fc9d
Paulo Santos <paulo.santos@buser.com.br>
2023-07-02 17:52:51 -0300

    - Creates App.js to be broken in small components - Adds bootstrap on index.html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,203 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+});
 
 function App() {
+  const [currentUser, setCurrentUser] = useState();
+  const [registrationToggle, setRegistrationToggle] = useState(false);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    client
+      .get("api/profile")
+      .then(function (res) {
+        setCurrentUser(true);
+      })
+      .catch(function (error) {
+        setCurrentUser(false);
+      });
+  }, []);
+
+  function updateForm() {
+    let toggleBtn = document.getElementById("toggle-btn");
+
+    registrationToggle
+      ? (toggleBtn.innerHTML = "Cadastrar")
+      : (toggleBtn.innerHTML = "Logar");
+    setRegistrationToggle(!registrationToggle);
+  }
+
+  function submitRegistration(e) {
+    e.preventDefault();
+    client
+      .post("api/register", {
+        name: name,
+        username: username,
+        email: email,
+        password: password,
+      })
+      .then(function (res) {
+        client
+          .post("api/login", {
+            email: email,
+            password: password,
+          })
+          .then(function (res) {
+            setCurrentUser(true);
+          });
+      });
+  }
+
+  function submitLogin(e) {
+    e.preventDefault();
+    client
+      .post("api/login", {
+        email: email,
+        password: password,
+      })
+      .then(function (res) {
+        setCurrentUser(true);
+      });
+  }
+
+  function submitLogout(e) {
+    e.preventDefault();
+    client.post("api/logout", { withCredentials: true }).then(function (res) {
+      setCurrentUser(false);
+    });
+  }
+
+  if (currentUser) {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>Prorius</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <form onSubmit={(e) => submitLogout(e)}>
+                  <Button type="submit" variant="light">
+                    Sair
+                  </Button>
+                </form>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div className="center">
+          <h2>Você está logado!!!</h2>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>Prorius</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <Button id="toggle-btn" onClick={updateForm} variant="light">
+                Cadastrar
+              </Button>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {registrationToggle ? (
+        <div className="center">
+          <Form onSubmit={(e) => submitRegistration(e)}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Insira seu nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Nome de usuário</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Insira um nome de usuário"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Endereço de email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Insira um email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Insira uma senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
+          </Form>
+        </div>
+      ) : (
+        <div className="center">
+          <Form onSubmit={(e) => submitLogin(e)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Endereço de email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Insira seu email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
+          </Form>
+        </div>
+      )}
     </div>
   );
 }
