53be892cd173ce3574f53d52f85832cc5995944b
Paulo Santos <paulo.santos@buser.com.br>
2023-07-04 15:40:49 -0300

    - Removes unused imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 
 import NavBar from "./components/Navbar";
 import WelcomeUser from "./components/WelcomeUser";
